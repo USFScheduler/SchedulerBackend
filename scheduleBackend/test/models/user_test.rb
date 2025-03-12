44d25749444134000b141e5e15ae38b032325e94
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2025-03-12 13:10:47 -0400

    Created a new test case "user_test.rb"
    This test case checks for users to not have duplicate emails, and checks for a user to not be saved without a password

--- scheduleBackend/test/models/user_test.rb
+++ scheduleBackend/test/models/user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User's email is unique" do
+    user1 = User.new(name: "John", email: "john@test.com", password: "password")
+    user1.save
+    user2 = User.new(name: "John Two", email: "john@test.com", password: "password")
+    assert_not user2.valid?, "User with duplicate email was valid"
+  end
+
+  test "User's did not provide a password" do
+    user1 = User.new(name: "John", email: "skbidi@bruh.com", password: "password")
+    user1.save
+    user2 = User.new(name: "John Two", email: "skibidhruh@bruh.com", password: "")
+    assert_not user2.valid?, "User does not have a password"
+  end
 end

b996bbd04f4114c8c1564c77a1256453c3232e5c
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2025-03-25 21:52:42 -0400

    Created the backend MVC model for users (registration, sign in)

--- scheduleBackend/config/routes.rb
+++ scheduleBackend/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show, :create, :update, :destroy]
   resources :tasks, only: [:index, :show, :create, :update, :destroy]
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy
+  post '/login', to: 'users#login'
+  delete 'logout', to: 'sessions#destroy'
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
